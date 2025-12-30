413ca648b072c5d6499258df80f1f48d0e618853
wyatt4543 <w4543w@gmail.com>
2025-12-30 15:53:55 -0600

    Update #FF0000.lua

--- #FF0000.lua
+++ #FF0000.lua
@@ -23,4 +23,27 @@ SMODS.Joker {
 			"Copies ability of {C:attention}Joker{} to the left"
 		}
 	},
+	rarity = 3,
+	atlas = "JokerAtlas",
+	pos = { x = 0, y = 0 },
+	cost = 10,
+
+	calculate = function(self, card, context)
+        
+        	local target_joker = nil
+        
+        	local my_pos = nil
+        	for i = 1, #G.jokers.cards do
+            		if G.jokers.cards[i] == card then
+                		my_pos = i
+                		break
+            		end
+        	end
+        	target_joker = (my_pos and my_pos > 1) and G.jokers.cards[my_pos - 1] or nil
+        
+        	local ret = SMODS.blueprint_effect(card, target_joker, context)
+        	if ret then
+            		SMODS.calculate_effect(ret, card)
+        	end
+    	end
 }
