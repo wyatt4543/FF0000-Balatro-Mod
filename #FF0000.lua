0a5c6a89e930f3e095c6ec46b8c20024c8755d68
wyatt4543 <w4543w@gmail.com>
2025-12-30 15:08:53 -0600

    Update #FF0000.lua

--- #FF0000.lua
+++ #FF0000.lua
@@ -1,3 +1,20 @@
 --[[
 comments go here I guess
 --]]
+
+--Create the joker atlas
+SMODS.Atlas {
+	-- Key for code to find it with
+	key = "JokerAtlas",
+	-- The name of the file, for the code to pull the atlas from
+	path = "JokerAtlas.png",
+	-- Width of each sprite in 1x size
+	px = 138,
+	-- Height of each sprite in 1x size
+	py = 186
+}
+
+--Yellowprint joker
+SMODS.Joker {
+
+}
