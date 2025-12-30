c42e3576b1120be4edc00dc6dd2ebb2ddf6073af
wyatt4543 <w4543w@gmail.com>
2025-12-30 17:02:46 -0600

    Update #FF0000.lua

--- #FF0000.lua
+++ #FF0000.lua
@@ -22,6 +22,42 @@ SMODS.Atlas {
     py = 34
 }
 
+--Dad Braggin' joker
+SMODS.Joker {
+	key = 'dadbraggin',
+	loc_txt = {
+		name = "Dad Braggin'",
+		text = {
+			"Each {C:attention}King{} gives {C:money}$#1#{} when scored",
+			"{X:mult,C:white} X#2# {} Mult for each modifier it has"
+		}
+	},
+	config = { extra = { money = 1, Xmult = 2 } },
+	rarity = 3,
+	atlas = "JokerAtlas",
+	pos = { x = 1, y = 0 },
+	cost = 8,
+
+	calculate = function(self, card, context)
+        
+        	local target_joker = nil
+        
+        	local my_pos = nil
+        	for i = 1, #G.jokers.cards do
+            		if G.jokers.cards[i] == card then
+                		my_pos = i
+                		break
+            		end
+        	end
+        	target_joker = (my_pos and my_pos > 1) and G.jokers.cards[my_pos - 1] or nil
+        
+        	local ret = SMODS.blueprint_effect(card, target_joker, context)
+        	if ret then
+            		SMODS.calculate_effect(ret, card)
+        	end
+    	end
+}
+
 --Yellowprint joker
 SMODS.Joker {
 	key = 'yellowprint',
