8f226ca863cb4ab549f89747c6cf2ec08570d854
wyatt4543 <w4543w@gmail.com>
2025-12-30 17:32:31 -0600

    Update #FF0000.lua

--- #FF0000.lua
+++ #FF0000.lua
@@ -42,21 +42,24 @@ SMODS.Joker {
 	end,
 
 	calculate = function(self, card, context)
-        
-        	local target_joker = nil
-        
-        	local my_pos = nil
-        	for i = 1, #G.jokers.cards do
-            		if G.jokers.cards[i] == card then
-                		my_pos = i
-                		break
+        	if context.individual and context.cardarea == G.play  then
+            		if context.other_card:get_id() == 13 then
+                		return {
+                    
+                    			func = function()
+                        
+                        			local current_dollars = G.GAME.dollars
+                        			local target_dollars = G.GAME.dollars + 1
+                        			local dollar_value = target_dollars - current_dollars
+                        			ease_dollars(dollar_value)
+                        			card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "+"..tostring(1), colour = G.C.MONEY})
+                        			return true
+                    			end,
+                    			extra = {
+                        			Xmult = 2
+                    			}
+                		}
             		end
-        	end
-        	target_joker = (my_pos and my_pos > 1) and G.jokers.cards[my_pos - 1] or nil
-        
-        	local ret = SMODS.blueprint_effect(card, target_joker, context)
-        	if ret then
-            		SMODS.calculate_effect(ret, card)
         	end
     	end
 }
